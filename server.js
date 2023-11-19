8f5ce4156eb5af653913e6f22ddcaae413a9d6b8
lkrusso <lararusso.uk@gmail.com>
2023-11-19 15:28:02 +0000

    fix: Fix GET request to correctly use user-controller.js and user-routes.js

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 require("dotenv").config();
 const knex = require("knex")(require("./knexfile"));
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const userRoutes = require("./controllers/user-controller");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.listen(process.env.SERVER_PORT);
