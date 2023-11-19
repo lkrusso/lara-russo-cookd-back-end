94ef6d0ebfe6e2d8edcb7a46ff18cf412dbc0edf
lkrusso <lararusso.uk@gmail.com>
2023-11-19 15:06:57 +0000

    fix: Fix error in ID on recipe seed

--- seeds/03_recipes.js
+++ seeds/03_recipes.js
@@ -25,7 +25,7 @@ exports.seed = async function (knex) {
       cuisine_type: "western",
     },
     {
-      id: 2,
+      id: 3,
       user_id: 1,
       title: "turkey chilli",
       duration: 65,
