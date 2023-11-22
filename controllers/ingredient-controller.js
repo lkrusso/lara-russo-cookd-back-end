f62e70ac866e15e6d85f36987e3b8cf64392c70e
lkrusso <lararusso.uk@gmail.com>
2023-11-22 12:28:44 +0000

    feat: Functionality to add ingredients to DB via POST request, with correct corresponding recipe ID

--- controllers/ingredient-controller.js
+++ controllers/ingredient-controller.js
@@ -2,15 +2,33 @@ const knex = require("knex")(require("../knexfile"));
 
 const getIngredients = async (req, res) => {
   try {
-    if (!req.body.id) {
+    if (!req.body.recipe_id) {
       return res.status(400).send("Please include the ID of the recipe");
     }
-    const ingredients = await knex("ingredients").where({
-      recipe_id: req.body.id,
+
+    const allRecipes = await knex("recipes");
+    let isMatchingRecipe = false;
+
+    allRecipes.forEach((recipe) => {
+      if (recipe.id === req.body.recipe_id) {
+        isMatchingRecipe = true;
+      }
     });
+
+    if (!isMatchingRecipe) {
+      return res
+        .status(400)
+        .send("The recipe ID does not exist in the DB. Please try again");
+    }
+
+    const ingredients = await knex("ingredients").where({
+      recipe_id: req.body.recipe_id,
+    });
+
     if (ingredients.length === 0) {
       return res.status(404).send("No ingredients found");
     }
+
     res.status(201).send(ingredients);
   } catch (error) {
     console.error(error);
@@ -22,4 +40,52 @@ const getIngredients = async (req, res) => {
   }
 };
 
-module.exports = { getIngredients };
+const createIngredients = async (req, res) => {
+  try {
+    const { recipe_id, ingredientList } = req.body;
+    console.log(ingredientList);
+    if (!recipe_id) {
+      return res.status(400).send("Please include the ID of the recipe");
+    }
+
+    if (!ingredientList) {
+      return res
+        .status(400)
+        .send("Please include the ingredients of the recipe");
+    }
+
+    const validRecipeID = await knex("recipes").where({ id: recipe_id });
+    if (!validRecipeID) {
+      return res
+        .status(400)
+        .send("The recipe ID sent was not valid. Please try again");
+    }
+
+    let ingredientDetails = {};
+
+    for (let i = 0; i < ingredientList.length; i++) {
+      ingredientDetails = {
+        recipe_id: recipe_id,
+        name: ingredientList[i].ingredient,
+        quantity: ingredientList[i].quantity,
+      };
+
+      await knex("ingredients").insert(ingredientDetails);
+    }
+
+    const addedIngredients = await knex("ingredients").where({
+      recipe_id: recipe_id,
+    });
+
+    return res.status(200).send(addedIngredients);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .send(
+        `Unable to create ingredients for recipe with ID ${req.body.recipe_id} due to server-side error`
+      );
+  }
+};
+
+module.exports = { getIngredients, createIngredients };
--- routes/ingredient-routes.js
+++ routes/ingredient-routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const ingredientController = require("../controllers/ingredient-controller");
 
 router.route("/").post(ingredientController.getIngredients);
+router.route("/add").post(ingredientController.createIngredients);
 
 module.exports = router;
