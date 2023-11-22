76c68a2c6488cacc92028d790931fef38ec34ca2
lkrusso <lararusso.uk@gmail.com>
2023-11-22 09:38:04 +0000

    feat: Functionality to add a new recipe to DB using POST request, and return the recipe ID upon successful completion

--- controllers/recipe-controller.js
+++ controllers/recipe-controller.js
@@ -20,4 +20,59 @@ const getAllRecipes = async (req, res) => {
   }
 };
 
-module.exports = { getAllRecipes };
+const createRecipe = async (req, res) => {
+  try {
+    const { user_id, title, duration, serves, cuisine_type } = req.body;
+
+    if (!user_id || !title || !duration || !serves || !cuisine_type) {
+      return res
+        .status(400)
+        .send("Please ensure you have provided all information necessary");
+    }
+
+    if (
+      typeof user_id !== "number" ||
+      typeof duration !== "number" ||
+      typeof serves !== "number"
+    ) {
+      return res
+        .status(400)
+        .send("Please ensure that IDs, duration and serves are all numbers");
+    }
+
+    const allUsers = await knex("users");
+    let isMatchingUser = false;
+
+    allUsers.forEach((user) => {
+      if (user.id === user_id) {
+        isMatchingUser = true;
+      }
+    });
+
+    if (!isMatchingUser) {
+      return res
+        .status(400)
+        .send("The user ID does not match. Please try again");
+    }
+
+    const newRecipe = { user_id, title, duration, serves, cuisine_type };
+
+    await knex("recipes").insert(newRecipe);
+    const addedRecipeID = await knex("recipes")
+      .where({
+        user_id: newRecipe.user_id,
+      })
+      .where({ title: newRecipe.title })
+      .where({ duration: newRecipe.duration })
+      .where({ serves: newRecipe.serves })
+      .where({ cuisine_type: newRecipe.cuisine_type })
+      .select("id");
+    res.status(200).send(addedRecipeID);
+  } catch (error) {
+    return res
+      .status(500)
+      .send(`Unable to create a new recipe due to server-side error: ${error}`);
+  }
+};
+
+module.exports = { getAllRecipes, createRecipe };
--- routes/recipe-routes.js
+++ routes/recipe-routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const recipeController = require("../controllers/recipe-controller");
 
 router.route("/").post(recipeController.getAllRecipes);
+router.route("/add").post(recipeController.createRecipe);
 
 module.exports = router;
