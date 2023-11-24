7e4cee21f874e3901fea4fc24007017f105cb7b3
lkrusso <lararusso.uk@gmail.com>
2023-11-24 10:56:30 +0000

    fix: Refactor getInstructions from POST to GET request using params

--- controllers/instruction-controller.js
+++ controllers/instruction-controller.js
@@ -2,14 +2,16 @@ const knex = require("knex")(require("../knexfile"));
 
 const getInstructions = async (req, res) => {
   try {
-    if (!req.body.id) {
+    if (!req.params.id) {
       return res.status(400).send("Please include the ID of the recipe");
     }
     const instructions = await knex("instructions").where({
-      recipe_id: req.body.id,
+      recipe_id: req.params.id,
     });
     if (instructions.length === 0) {
-      return res.status(404).send("No instructions found");
+      return res
+        .status(404)
+        .send(`No instructions found for recipe with ID ${req.params.id}`);
     }
     res.status(201).send(instructions);
   } catch (error) {
@@ -17,7 +19,7 @@ const getInstructions = async (req, res) => {
     return res
       .status(500)
       .send(
-        `Unable to get instructions for recipe with ID ${req.body.id} due to server-side error`
+        `Unable to get instructions for recipe with ID ${req.params.id} due to server-side error`
       );
   }
 };
--- routes/instruction-routes.js
+++ routes/instruction-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const instructionController = require("../controllers/instruction-controller");
 
-router.route("/").post(instructionController.getInstructions);
+router.route("/:id").get(instructionController.getInstructions);
 router.route("/add").post(instructionController.createInstructions);
 router.route("/edit").patch(instructionController.updateInstructions);
 
