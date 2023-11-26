4b8b9dba09ae6a0053efed721a10c7dce5ce1466
lkrusso <lararusso.uk@gmail.com>
2023-11-26 22:25:03 +0000

    fix: Fix seed files to have encrypted passwords and use real authentication

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -6,8 +6,20 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("users").del();
   await knex("users").insert([
-    { id: 1, username: "test@test.com", password: "test" },
-    { id: 2, username: "jsmith@gmail.com", password: "password" },
-    { id: 3, username: "bob@bob.com", password: "password123" },
+    {
+      id: 1,
+      username: "test@test.com",
+      password: "$2a$12$rFQAOEhOQYmbuN9TYZDAK.AwcDPq/Zgduhc9Puud5MmyPbeTqpgx6",
+    },
+    {
+      id: 2,
+      username: "jsmith@gmail.com",
+      password: "$2a$12$jJNHGV68hF2kMGgTzl5.gOcor9Bl7a.I4omZpSUj8g.chyem2lrEO",
+    },
+    {
+      id: 3,
+      username: "bob@bob.com",
+      password: "$2a$12$jRPhFI9DsvA3ZcGTlF0REeIaTRbmQpikU5AuZf/Zdi7v0OK3pRAGq",
+    },
   ]);
 };
