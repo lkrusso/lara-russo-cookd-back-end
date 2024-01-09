1efb781e1d0e08b0d0bdcdc46d0d54d794f84c63
lkrusso <lararusso.uk@gmail.com>
2024-01-09 14:13:53 +0000

    feat: Create POST request to create a new review

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -25,4 +25,48 @@ const getSingleReview = async (req, res) => {
   }
 };
 
-module.exports = { getSingleReview };
+const createReview = async (req, res) => {
+  try {
+    const { recipe_id, rating, comment } = req.body;
+    if (!recipe_id || !rating || !comment) {
+      return res
+        .status(400)
+        .send("Please ensure you have provided all the information necessary");
+    }
+    if (typeof recipe_id !== "number" || typeof rating !== "number") {
+      return res
+        .status(400)
+        .send("Please ensure that IDs and ratings are numbers");
+    }
+    if (typeof comment !== "string") {
+      return res.status(400).send("Please ensure that the comment is a string");
+    }
+
+    const isExistingRecipe = await knex("recipes").where({ id: recipe_id });
+    console.log(isExistingRecipe);
+    if (isExistingRecipe.length === 0) {
+      return res.status(404).send(`Recipe with ID ${recipe_id} not found`);
+    }
+
+    const isExistingReview = await knex("reviews").where({
+      recipe_id: recipe_id,
+    });
+    console.log(isExistingReview);
+    if (isExistingReview.length !== 0) {
+      return res
+        .status(400)
+        .send(`Review for recipe with ID ${recipe_id} already exists`);
+    }
+
+    const newReview = { recipe_id, rating, comment };
+    await knex("reviews").insert(newReview);
+    const addedReview = await knex("reviews").where({ recipe_id: recipe_id });
+    return res.status(200).send(addedReview);
+  } catch (error) {
+    return res
+      .status(500)
+      .send(`Unable to create a new review due to server-side error`);
+  }
+};
+
+module.exports = { getSingleReview, createReview };
--- routes/review-routes.js
+++ routes/review-routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const reviewController = require("../controllers/review-controller");
 
 router.route("/:id").get(reviewController.getSingleReview);
+router.route("/add").post(reviewController.createReview);
 
 module.exports = router;
